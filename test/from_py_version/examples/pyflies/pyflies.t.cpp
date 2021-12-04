d37b024b609d230d37a6d4c2002a0117451eb87d
goto40 <pierre.bayerl@googlemail.com>
2021-12-04 17:52:18 +0100

    pyflies no ok for me

--- test/from_py_version/examples/pyflies/pyflies.t.cpp
+++ test/from_py_version/examples/pyflies/pyflies.t.cpp
@@ -11,4 +11,7 @@ TEST_CASE("from_python/examples/pyflies1", "[textx/from_python/examples]")
 
     auto p_model1 = std::filesystem::path(__FILE__).parent_path().append("experiment.pf");
     auto m1 = mm->model_from_file(p_model1);
+
+    auto p_model2 = std::filesystem::path(__FILE__).parent_path().append("order_changed_of_optional_element_in_unordered_group.pf");
+    auto m2 = mm->model_from_file(p_model2);
 }
