dbb70a2eb6e776683c7be4ced4c7250d73d21554
goto40 <pierre.bayerl@googlemail.com>
2021-12-22 17:03:29 +0100

    alternative generator type explored

---
+++ examples/coro0.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <coroutine>
+
+/* adapted from https://www.scs.stanford.edu/~dm/blog/c++-coroutines.html */
+template <typename T>
+struct Generator
+{
+    struct promise_type;
+    using handle_type = std::coroutine_handle<promise_type>;
+
+    struct promise_type
+    {
+        T value_;
+        std::exception_ptr exception_;
+
+        Generator get_return_object()
+        {
+            return Generator(handle_type::from_promise(*this));
+        }
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void unhandled_exception() { exception_ = std::current_exception(); }
+        template <std::convertible_to<T> From> // C++20 concept
+        std::suspend_always yield_value(From &&from)
+        {
+            value_ = std::forward<From>(from);
+            return {};
+        }
+        void return_void() {}
+    };
+
+    handle_type h_;
+
+    Generator(handle_type h) : h_(h) {}
+    ~Generator() { h_.destroy(); }
+    explicit operator bool()
+    {
+        fill();
+        return !h_.done();
+    }
+    T operator()()
+    {
+        fill();
+        full_ = false;
+        return std::move(h_.promise().value_);
+    }
+
+    struct Iterator {
+        Generator& g;
+        bool operator!=(std::nullptr_t) { return static_cast<bool>(g); }
+        void operator++() {}
+        T operator*() { return g(); }
+    };
+    Iterator begin() { return Iterator{*this}; }
+    std::nullptr_t end() { return nullptr; }
+
+private:
+    bool full_ = false;
+
+    void fill()
+    {
+        if (!full_)
+        {
+            h_();
+            if (h_.promise().exception_)
+                std::rethrow_exception(h_.promise().exception_);
+            full_ = true;
+        }
+    }
+};
+
+Generator<unsigned>
+counter6()
+{
+    for (unsigned i = 0; i < 3;)
+        co_yield i++;
+}
+
+int main()
+{
+    {
+        auto gen = counter6();
+        while (gen)
+            std::cout << "counter6: " << gen() << std::endl;
+    }
+    {
+        for (auto x: counter6()) {
+            std::cout << "counter6-for: " << x << std::endl;
+        }
+    }
+}
