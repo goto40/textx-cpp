5861cb418d8726a4e865fea8ea4082156969e27c
goto40 <pierre.bayerl@googlemail.com>
2022-01-10 19:37:38 +0100

    doc

--- src/textx/istrings.h
+++ src/textx/istrings.h
@@ -7,5 +7,18 @@ namespace textx::istrings {
     using ExternalLink = std::variant<Obj2StrFun, std::shared_ptr<textx::object::Object>>;
 
     std::shared_ptr<textx::Workspace> get_istrings_metamodel_workspace();
+
+    /**
+     * @brief format an indented string
+     * You can use <% obj.attr %> to print str-based attributes.
+     * You can use <% FOR x: obj.lst %> ... <% ENDFOR %> (inspired by xtend).
+     *  - Indentation is adapted and indentations introduced in for-loops are ignored.
+     * You can use <% fun(obj) %> to call provided object-to-string conversion functions. 
+     *  - Indentation is propagated into multi-line string outputs of the conversion functions. 
+     * 
+     * @param istring_text a string the the iformat language
+     * @param external_links a map: name --> (shared_ptr<object>|string(shared_ptr<object>))
+     * @return std::string 
+     */
     std::string i(std::string istring_text, std::unordered_map<std::string,ExternalLink> external_links);
 }
