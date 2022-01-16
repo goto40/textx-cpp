f25be992dc86b8027859a197122cdfc7ed6a8622
goto40 <pierre.bayerl@googlemail.com>
2022-01-16 11:33:36 +0100

    wip

--- examples/itemlang/demo0.ts
+++ examples/itemlang/demo0.ts
@@ -1,3 +1,4 @@
+import { join, isAbsolute, normalize, dirname } from "https://deno.land/std@0.102.0/path/mod.ts";
 
 function assert(condition: any, msg?: string): asserts condition {
     if (!condition) {
@@ -31,15 +32,19 @@ function resolve_path(obj: object, path: string): object {
     return obj;
 }
 
-async function load(fn: string): Promise<object> {
-
+async function load(fn_main: string): Promise<object> {
+    if (!isAbsolute(fn_main)) {
+        fn_main = join(Deno.cwd(), fn_main);
+    }
+    fn_main = normalize(fn_main);
     const decoder = new TextDecoder('utf-8')
-    const data = await Deno.readFile(fn)
-    //console.log("processing "+fn)
-    var root_obj = JSON.parse(decoder.decode(data))
+    const data = await Deno.readFile(fn_main)
+    console.log("processing "+fn_main)
+    let root_obj = JSON.parse(decoder.decode(data))
+    let all_files = new Map<string, object>();
 
-    function inner_resolve(obj: any) {
-        Object.keys(obj).some(function(k) {
+    async function inner_resolve(obj: any, fn_base: string): Promise<object> {
+        Object.keys(obj).some(async function(k) {
             if (k === "$ref") {
                 //console.log("$ref found in "+obj+" : " + obj['$ref'])
                 let [uri, ref] = obj['$ref'].split('#');
@@ -48,22 +53,39 @@ async function load(fn: string): Promise<object> {
                     return resolve_path(root_obj,ref);
                 }
                 else {
-                    console.log(uri+","+ref)
+                    let fn = uri;
+                    if (!isAbsolute(fn)) {
+                        fn = join(dirname(fn_base), fn);
+                    }
+                    fn = normalize(fn)
+                    let other_obj: object = {};
+                    if (!(fn in all_files)) {
+                        const decoder = new TextDecoder('utf-8')
+                        const data = await Deno.readFile(fn)
+                        console.log("inner_processing "+fn)
+                        other_obj = JSON.parse(decoder.decode(data))
+                        all_files.set(fn, other_obj)
+                        all_files.set(fn, inner_resolve(other_obj, fn));
+                    }
+                    else {
+                        other_obj = all_files.get(fn) as object;
+                    }
+                    return resolve_path(other_obj,ref);
                 }
                 //value = obj[k];
                 return obj;
             }
             if (obj[k] && typeof obj[k] === 'object') {
-                obj[k] == inner_resolve(obj[k]);
+                obj[k] == inner_resolve(obj[k], fn_base);
             }
         });
         return obj
     }
 
-    root_obj = inner_resolve(root_obj)
+    all_files.set(fn_main, root_obj);
+    all_files.set(fn_main, inner_resolve(root_obj, fn_main));
     return root_obj
 }
 
-let fn = Deno.args[0]
-let obj = await load(fn)
-//console.log(obj)
+let obj = await load(Deno.args[0])
+console.log(obj)
