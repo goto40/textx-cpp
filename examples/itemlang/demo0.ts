ea5a7c64067bb5965ea5b91ce751a39add092614
goto40 <pierre.bayerl@googlemail.com>
2022-01-17 22:27:06 +0100

    wip

--- examples/itemlang/demo0.ts
+++ examples/itemlang/demo0.ts
@@ -1,12 +1,17 @@
 import * as TextxObj from "../../typescript_tools/ref_resolver/mod.ts";
 import * as Eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
+import { join } from "https://deno.land/std@0.102.0/path/mod.ts";
 
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const simpleEta = join(__dirname, "simple.eta");
 let obj = await TextxObj.load(Deno.args[0])
 //console.log(obj)
 
-let structs = TextxObj.findObjByTypeype(obj, "Struct");
+let structs = TextxObj.findObjByTypeype(obj, "Struct") as any;
 for(let i=0;i<structs.length;i++) {
-    console.log( structs[i] );
+    console.log( structs[i].name );
+    let code = await Eta.renderFile(simpleEta, structs[i]) as string;
+    await Deno.writeTextFile(structs[i].name+".h", code);
 }
 
-//Eta.renderFile("simple.eta", obj);
--- examples/itemlang/run_demo0.sh
+++ examples/itemlang/run_demo0.sh
@@ -1,3 +1,3 @@
  
 ./itemlang ../examples/itemlang/model/big_example.item
-deno run --allow-read ../examples/itemlang/demo0.ts big_example.json 
+deno run --allow-write --allow-read ../examples/itemlang/demo0.ts big_example.json 
