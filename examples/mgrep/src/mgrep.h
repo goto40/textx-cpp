01012593e3c06205c23c2d226a31ff02ac55e8e5
goto40 <pierre.bayerl@googlemail.com>
2022-02-20 15:47:48 +0000

    speedu 400 -> 140

--- examples/mgrep/src/mgrep.h
+++ examples/mgrep/src/mgrep.h
@@ -1,5 +1,6 @@
 #include <textx/object.h>
 #include <textx/workspace.h>
+#include <textx/istrings.h>
 #include <memory>
 #include <deque>
 
@@ -11,12 +12,18 @@ namespace mgrep {
 
     class MGrep {
         std::shared_ptr<textx::Workspace> workspace = textx::Workspace::create();
+        std::shared_ptr<textx::Model> istr_cmd={}, istr_data={};
+        std::unordered_map<std::string,textx::istrings::ExternalLink> external_links={};
         size_t line = 1;
         std::deque<MGrepMatch> m_matches = {};
         std::optional<std::string> m_transform_command;
     public:
         MGrep(std::string model);
-        void set_transform_command(std::string c) { m_transform_command=c; }
+        void set_transform_command(std::string c) { 
+            m_transform_command = c; 
+            auto mm = textx::istrings::get_istrings_metamodel_workspace();
+            istr_cmd = mm->model_from_str(c);
+        }
         bool parse_and_store(std::string text);
         std::optional<std::string> parse_and_transform(std::string text, std::string filename="");
         const auto &matches() { return m_matches; }
